0875ab0c5f5a21e307124633cc1e02ef4a3e17fe
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-26 19:45:13 -0500

    stuck on form

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const gameList =   document.querySelector('.game-list')
+const mainGame = document.querySelector('.game-details')
+const gameImage = document.querySelector('#detail-image')
+const gameName = document.querySelector('#detail-title')
+
+fetch('http://localhost:3000/games')
+.then(r => r.json())
+.then(gamesArray => {
+    gamesArray.forEach(game =>{
+        renderGameNames(game)
+        renderMain(gamesArray[0])
+    })
+})
+
+
+function renderGameNames(gameListObj) {
+    const games = document.createElement('h5')
+    games.textContent = `${gameListObj.name} (${gameListObj.manufacturer_name})`
+    gameList.append(games)
+    games.addEventListener('click', (e) =>{
+        renderMain(gameListObj)
+    })
+}
+function renderMain(gameObj){
+    gameImage.src = gameObj.image
+    gameName.textContent = gameObj.name
+    highScore.textContent = gameObj.high_score
+    mainGame.appendChild(gameImage, gameName, highScore)
+}
+const formHigh = document.querySelector('#high-score-form')
+const highScore = document.querySelector('#detail-high-score')
+const highScoreInput = document.querySelector('#score-input')
+
+formHigh.addEventListener('sumbit', (e) => {
+    e.preventDefault()
+    console.log(e)
+})
